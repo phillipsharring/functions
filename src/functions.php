5fc1238e97a0f52bc2a9720c7c4009e1650bd757
Phillip Harrington <phharrin@cisco.com>
2016-03-08 12:57:13 -0500

    updating composer

--- src/functions.php
+++ src/functions.php
@@ -1,6 +1,20 @@
 <?php
 
-function protocol()
+use Phharrin\Functions\Debug\Dumper;
+
+if (!function_exists('protocol')) {
+    function protocol()
+    {
+        return !isset($_SERVER['HTTPS']) || ('off' == $_SERVER['HTTPS']) ? 'http://' : 'https://';
+    }
+}
+
+if (!function_exists('d'))
 {
-    return !isset($_SERVER['HTTPS']) || ('off' == $_SERVER['HTTPS']) ? 'http://' : 'https://';
+    function d()
+    {
+        array_map(function ($x) {
+            (new Dumper)->dump($x);
+        }, func_get_args());
+    }
 }
